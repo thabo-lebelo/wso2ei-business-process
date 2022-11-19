76078dd45ed9b6b4435bb4a40fae8bbd5ae63b69
Thabo Lebelo <thabolebelo@icloud.com>
2022-11-19 23:23:50 +0200

    Code cleanup

--- bin/wso2ei-business-process.ts
+++ bin/wso2ei-business-process.ts
@@ -6,6 +6,6 @@ import { Wso2EiBusinessProcessStack } from '../lib/wso2ei-business-process-stack
 const app = new cdk.App();
 new Wso2EiBusinessProcessStack(app, 'Wso2EiBusinessProcessStack', {
     env: { account: '737327749629', region: 'us-east-1' },
-    stackName: 'wso2ei-business-process',
+    stackName: 'wso2-business-process',
     description: 'Business Process Server profile instance'
 });
--- lib/wso2ei-business-process-stack.ts
+++ lib/wso2ei-business-process-stack.ts
@@ -7,28 +7,24 @@ import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
-import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export class Wso2EiBusinessProcessStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        // VPC and LB
-
         const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
             vpcName: 'Blog',
         });
 
-        // Cluster to deploy resources to
         const cluster = new ecs.Cluster(this, "Cluster", {
             vpc: vpc,
-            clusterName: "wso2ei-business-process",
+            clusterName: "wso2-business-process",
         });
 
         const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
             vpc: vpc,
             internetFacing: true,
-            loadBalancerName: 'WSO2'
+            loadBalancerName: 'wso2-business-process'
         });
 
         /* DNS, DOMAINS, CERTS */
@@ -38,8 +34,7 @@ export class Wso2EiBusinessProcessStack extends Stack {
         });
 
         const cert = new acm.Certificate(this, 'thabolebelo', {
-            domainName: 'wso2.thabolebelo.com',
-            subjectAlternativeNames: ['*.wso2.thabolebelo.com'],
+            domainName: 'wso2-business-process.thabolebelo.com',
             validation: acm.CertificateValidation.fromDns(zone)
         });
 
@@ -51,28 +46,26 @@ export class Wso2EiBusinessProcessStack extends Stack {
             ),
             ttl: Duration.seconds(300),
             comment: 'URL to access the instance',
-            recordName: 'wso2'
+            recordName: 'wso2-business-process'
         });
 
-        // Docker repo for bps image
         const repo = ecr.Repository.fromRepositoryArn(this, "Repo",
             "arn:aws:ecr:us-east-1:737327749629:repository/wso2ei-business-process"
         );
-        const image = ecs.ContainerImage.fromEcrRepository(repo, 'latest')
 
-        // Task definition
-        const task = new ecs.TaskDefinition(this, 'TaskDef', {
+        const image = ecs.ContainerImage.fromEcrRepository(repo, '6.4.0')
+
+        const task = new ecs.TaskDefinition(this, 'Task', {
             cpu: "512",
             memoryMiB: "1024",
             compatibility: ecs.Compatibility.EC2_AND_FARGATE,
             networkMode: ecs.NetworkMode.AWS_VPC,
         });
 
-         // The docker container including the image to use
-         const container = task.addContainer('Container', {
+        const container = task.addContainer('Container', {
             memoryLimitMiB: 1024,
             image: image,
-            logging: ecs.LogDriver.awsLogs({ streamPrefix: "wso2" })
+            logging: ecs.LogDriver.awsLogs({ streamPrefix: "wso2-business-process" })
         });
 
         container.addPortMappings({
@@ -80,31 +73,33 @@ export class Wso2EiBusinessProcessStack extends Stack {
             protocol: ecs.Protocol.TCP
         });
 
-        container.addPortMappings({
-            containerPort: 9765,
-            protocol: ecs.Protocol.TCP
-        });
-
-        // create service
         const service = new ecs.FargateService(this, "Service", {
             cluster: cluster,
             taskDefinition: task,
-            serviceName: 'WSO2',
+            serviceName: 'business-process-profile',
+        });
+
+        const scaling = service.autoScaleTaskCount({ maxCapacity: 3, minCapacity: 1 });
+        
+        // Auto-Scaling depending on CPU utilization
+        scaling.scaleOnCpuUtilization('autoscale', {
+            targetUtilizationPercent: 50,
+            scaleInCooldown: Duration.minutes(2),
+            scaleOutCooldown: Duration.seconds(30)
         });
 
         /* CONFIGURE ALB DEFAULT LISTENERS */
-        const listener = alb.addListener('port9445Listener', { 
+        const listener = alb.addListener('port9445Listener', {
             port: 9445,
             certificates: [cert],
             protocol: elbv2.ApplicationProtocol.HTTPS
         });
-        
-        // Add target group to container
+
         listener.addTargets('service', {
             port: 9445,
             targets: [service],
             protocol: elbv2.ApplicationProtocol.HTTPS,
-            targetGroupName: 'WSO2',
+            targetGroupName: 'business-process-profile',
             healthCheck: {
                 path: '/services/Version',
                 protocol: elbv2.Protocol.HTTPS,
